8783da3409913108227559be91b21dc0674a269e
Daniil Zharuk <riddledwithknife@icloud.com>
2022-03-10 20:20:49 +0300

    Final

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import hashlib
-from time import time
-from unicodedata import name
+import multiprocessing
+import time
 
-start = time()
-n = int(input("Введите количество хэшей: "))
-hash = []
-for i in range(n):
-    hash.append(input())
 
-word = ""
-letters = list("abcdefghijklmnopqrstuvwxyz")
-result = {}
-for l1 in letters:
-    for l2 in letters:
-        for l3 in letters:
-            for l4 in letters:
-                for l5 in letters:
-                    word = bytes(l1 + l2 + l3 + l4 + l5, encoding='utf-8')
-                    if hashlib.sha256(word).hexdigest() in hash:
-                        result[hashlib.sha256(word).hexdigest()] = word
-print(result)
-end = time() - start
-print(end)
+def hash(hashes):
+    letters = list("abcdefghijklmnopqrstuvwxyz")
+    result = {}
+    for l1 in letters:
+        for l2 in letters:
+            for l3 in letters:
+                for l4 in letters:
+                    for l5 in letters:
+                        word = bytes(l1+l2+l3+l4+l5, encoding='utf-8')
+                        if hashlib.sha256(word).hexdigest() in hashes:
+                            result[hashlib.sha256(word).hexdigest()] = word
+    print(result)
+
+
+if __name__ == '__main__':
+    qwetions = int(input("Сколько хешей вы хотите ввести? "))
+    hashes = []
+    for i in range(0, qwetions):
+        hashes.append(str(input("Хэш: ")))
+        i += 1
+
+    ptch = input("Введите 1 для однопоточности, введите 2 для многопоточности: ")
+    if ptch == '1':
+        timer = time.time()
+
+        hash(hashes)
+
+        print("Время: ", time.time() - timer)
+    elif ptch == '2':
+        timer = time.time()
+
+        with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
+            p.map(hash, hashes)
+
+        print("Время: ", time.time() - timer)
